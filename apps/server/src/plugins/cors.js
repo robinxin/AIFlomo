56091270f4b67759e46e0ac5f93f0dd5b5579d29
linxin <linxin@yinxiang.com>
2026-03-05 20:11:59 +0800

    feat: update port to 8082 from 8081

--- apps/server/src/plugins/cors.js
+++ apps/server/src/plugins/cors.js
@@ -2,7 +2,7 @@ import fp from 'fastify-plugin';
 import fastifyCors from '@fastify/cors';
 
 export const corsPlugin = fp(async (fastify) => {
-  const corsOrigin = process.env.CORS_ORIGIN || 'http://localhost:8081';
+  const corsOrigin = process.env.CORS_ORIGIN || 'http://localhost:8082';
 
   await fastify.register(fastifyCors, {
     origin: corsOrigin,
