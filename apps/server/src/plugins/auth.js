54a0aacc8679ce56b6d8134343c8b93f1ec44fd8
gaomengxue <gaomengxue@yinxiang.com>
2026-03-06 10:43:54 +0800

    删除之前生成的代码

--- apps/server/src/plugins/auth.js
+++
@@ -1,9 +0,0 @@
-export async function requireAuth(request, reply) {
-  if (!request.session.userId) {
-    return reply.status(401).send({
-      data: null,
-      error: 'UNAUTHORIZED',
-      message: '请先登录',
-    });
-  }
-}
