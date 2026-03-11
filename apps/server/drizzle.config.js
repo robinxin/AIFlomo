2279b30cdebb8b5564a095b7b0333edcfafab271
Shuangshuang Li <lishuangshuang@yinxiang.com>
2026-03-11 19:20:56 +0800

    del code

--- apps/server/drizzle.config.js
+++
@@ -1,8 +0,0 @@
-export default {
-  schema: './src/db/schema.js',
-  out: './src/db/migrations',
-  dialect: 'sqlite',
-  dbCredentials: {
-    url: process.env.DB_PATH || './data/aiflomo.db',
-  },
-};
